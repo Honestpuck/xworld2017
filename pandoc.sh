994338fff911a45814cae53327380552178b9991
Tony Williams <honestpuck@gmail.com>
2017-05-29 09:05:23 +1000

    Beta of slides and file structure.

--- pandoc.sh
+++ pandoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pandoc -t revealjs notes/all.md -f markdown+pipe_tables --slide-level=3 -s -o index.html \
+pandoc -t revealjs slides.md -f markdown+pipe_tables --slide-level=3 -s -o index.html \
     -V theme=white \
     -V parallaxBackgroundImage="XW16-Empty.jpg" \
     -V parallaxBackgroundSize="1440px 900px" \
